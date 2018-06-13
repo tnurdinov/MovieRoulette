ccfa4f1353ae683f80c2aadc471671e1e2fb87bc
Temirlan Nurdinov <temirlan.nurdinov@accenture.com>
2018-06-13 23:16:45 +0300

    Added error handling, just catch error and print message

--- app/src/main/java/com/tnurdinov/movieroulette/MainActivity.kt
+++ app/src/main/java/com/tnurdinov/movieroulette/MainActivity.kt
@@ -29,19 +29,22 @@ class MainActivity : AppCompatActivity() {
         rating = findViewById(R.id.movie_rating)
         description = findViewById(R.id.movie_description)
 
-        val rand = Random()
-
         launch(UI) {
-            val ratedReponse = ThemoviedbService.create().getTopRatedMovies().await()
-            val firstMovie = ThemoviedbService.create().getMovieDetails(ratedReponse.results?.get(rand.nextInt(ratedReponse.results.size)+1)?.id!!).await()
+            try {
+                val ratedReponse = ThemoviedbService.create().getTopRatedMovies().await()
+                val movies = ratedReponse.results
+                val randomMovieId = movies?.get(Random().nextInt(movies.size - 1))?.id!!
+                val firstMovie = ThemoviedbService.create().getMovieDetails(randomMovieId).await()
+                Log.d("TAG", firstMovie.poster_path)
+                Picasso.get().load(BuildConfig.TMDB_IMG_URL + "w780${firstMovie.backdrop_path}").into(poster)
+                name?.text = firstMovie.title
+                year?.text = String.format(getString(R.string.release_date), firstMovie.release_date)
+                rating?.text = String.format(getString(R.string.rating), firstMovie.vote_average)
+                description?.text = firstMovie.overview
 
-            Log.d("TAG", firstMovie.poster_path)
-            Picasso.get().load(BuildConfig.TMDB_IMG_URL + "w780${firstMovie.backdrop_path}").into(poster)
-            name?.text = firstMovie.title
-            year?.text = String.format(getString(R.string.release_date), firstMovie.release_date)
-            rating?.text = String.format(getString(R.string.rating), firstMovie.vote_average)
-            description?.text = firstMovie.overview
+            } catch (e: Exception) {
+                Log.e("TAG", e.localizedMessage)
+            }
         }
-
     }
 }
