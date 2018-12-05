e2d8ed4634edaa59cd59c61399110f2f6dfa35ce
Temirlan Nurdinov <temirlan.nurdinov@accenture.com>
2018-12-06 00:08:45 +0200

    * Used syntheric class generated by kotlin * Disabled proguard

--- app/src/main/java/com/tnurdinov/movieroulette/MainActivity.kt
+++ app/src/main/java/com/tnurdinov/movieroulette/MainActivity.kt
@@ -3,8 +3,6 @@ package com.tnurdinov.movieroulette
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -12,21 +10,15 @@ import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.snackbar.Snackbar.LENGTH_SHORT
 import com.tnurdinov.movieroulette.model.MovieDetails
 import com.tnurdinov.movieroulette.viewmodel.MovieViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var poster: ImageView
-    private lateinit var name: TextView
-    private lateinit var year: TextView
-    private lateinit var rating: TextView
-    private lateinit var description: TextView
-    private lateinit var fab: FloatingActionButton
-    private var circularProgressDrawable:CircularProgressDrawable? = null
+    private var circularProgressDrawable: CircularProgressDrawable? = null
     private val PREFS_FILENAME = "com.teamtreehouse.colorsarefun.prefs"
     private val LAST_MOVIE_ID = "last_movie_id"
 
@@ -42,13 +34,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        poster = findViewById(R.id.movie_poster)
-        name = findViewById(R.id.movie_name)
-        year = findViewById(R.id.movie_year)
-        rating = findViewById(R.id.movie_rating)
-        description = findViewById(R.id.movie_description)
-        fab = findViewById(R.id.fab)
 
         fab.setOnClickListener {
             viewModel.getRandomMovie()
@@ -82,18 +67,21 @@ class MainActivity : AppCompatActivity() {
             Glide.with(this)
                     .load("${BuildConfig.TMDB_IMG_URL}w780${movie?.backdrop_path}")
                     .apply(options)
-                    .into(poster)
-            name.text = movie?.title
-            year.text = String.format(getString(R.string.release_date), movie?.release_date)
-            rating.text = String.format(getString(R.string.rating), movie?.vote_average)
-            description.text = movie?.overview
+                    .into(movie_backdrop)
+            Glide.with(this)
+                    .load("${BuildConfig.TMDB_IMG_URL}w342${movie?.poster_path}")
+                    .into(movie_poster)
+            movie_name.text = movie?.title
+            movie_year.text = String.format(getString(R.string.release_date), movie?.release_date)
+            movie_rating.text = String.format(getString(R.string.rating), movie?.vote_average)
+            movie_description.text = movie?.overview
         }
         viewModel.observeMovieDetails().observe(this, observer)
     }
 
     private fun observeError() {
         val observer = Observer<String> { errorMessage ->
-            Snackbar.make(poster, errorMessage, LENGTH_SHORT).show()
+            Snackbar.make(root_view, errorMessage, LENGTH_SHORT).show()
         }
 
 
